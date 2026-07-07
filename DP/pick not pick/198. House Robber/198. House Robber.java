6fec1e71c2c23fd3cad6537469fc2ceff2d799b2
Subashree S <subashreevjc@gmail.com>
2026-07-07 23:36:01 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 18.45%] - Leet2Hub

--- DP/pick not pick/198. House Robber/198. House Robber.java
+++ DP/pick not pick/198. House Robber/198. House Robber.java
@@ -1,18 +1,25 @@
 1class Solution {
 2    public int rob(int[] nums) {
-3        int n=nums.length;
-4        if(n==1){
-5            return nums[0];
-6        }
-7        int[] dp=new int[n];
-8        dp[0]=nums[0];
-9        dp[1]=Math.max(nums[1],nums[0]);
-10        for(int i=2;i<n;i++){
-11            int pick=nums[i]+dp[i-2];
-12            int notPick=dp[i-1];
+3
+4        int n = nums.length;
+5
+6        if (n == 1)
+7            return nums[0];
+8
+9        int prev2 = nums[0];
+10        int prev1 = Math.max(nums[0], nums[1]);
+11
+12        for (int i = 2; i < n; i++) {
 13
-14            dp[i]=Math.max(pick,notPick);
-15        }
-16        return dp[n-1];
-17    }
-18}
+14            int pick = nums[i] + prev2;
+15            int notPick = prev1;
+16
+17            int curr = Math.max(pick, notPick);
+18
+19            prev2 = prev1;
+20            prev1 = curr;
+21        }
+22
+23        return prev1;
+24    }
+25}
