4cb9d83d8e98b09bf8a4f7cdd189f5e70c03b0f0
Subashree S <subashreevjc@gmail.com>
2026-07-30 23:09:31 +0530

    [171] [Time Beats: 91.82%] [Memory Beats: 54.69%] - Leet2Hub

--- AMAZON/171. Excel Sheet Column Number/171. Excel Sheet Column Number.java
+++ AMAZON/171. Excel Sheet Column Number/171. Excel Sheet Column Number.java
@@ -1,13 +1,18 @@
 1class Solution {
-2    public int titleToNumber(String columnTitle) {
-3        long ans = 0;
+2
+3    public int titleToNumber(String columnTitle) {
 4
-5        // Same idea as decimal parsing, but base is 26.
-6        for (int i = 0; i < columnTitle.length(); i++) {
-7            char ch = columnTitle.charAt(i);
-8            ans = ans * 26 + (ch - 'A' + 1);
-9        }
+5        int ans = 0;
+6
+7        for (int i = 0; i < columnTitle.length(); i++) {
+8
+9            char ch = columnTitle.charAt(i);
 10
-11        return (int) ans;
-12    }
-13}
+11            int value = ch - 'A' + 1;
+12
+13            ans = ans * 26 + value;
+14        }
+15
+16        return ans;
+17    }
+18}
