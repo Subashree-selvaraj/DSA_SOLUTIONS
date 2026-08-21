5916e6ab20bb06e56bb8b0932667501770ca6ce0
Subashree S <subashreevjc@gmail.com>
2026-08-21 16:24:58 +0530

    [557] [Time Beats: 14.39%] [Memory Beats: 37.96%] - Leet2Hub

--- AMAZON/557. Reverse Words in a String III/557. Reverse Words in a String III.java
+++ AMAZON/557. Reverse Words in a String III/557. Reverse Words in a String III.java
@@ -1,15 +1,22 @@
 1class Solution {
 2    public String reverseWords(String s) {
-3        // Split the input string into words and reverse each word
-4        String[] words = s.split( );
-5        StringBuilder result = new StringBuilder();
-6        for (String word : words) {
-7            StringBuilder reversedWord = new StringBuilder(word).reverse();
-8            result.append(reversedWord).append( );
-9        }
-10        result.deleteCharAt(result.length() - 1);  // Remove the extra space at the end
-11        
-12        // Convert StringBuilder to String
-13        return result.toString();        
-14    }
-15}
+3        StringBuilder result = new StringBuilder();
+4        StringBuilder word = new StringBuilder();
+5
+6        for (int i = 0; i < s.length(); i++) {
+7            char ch = s.charAt(i);
+8            
+9            if (ch != ' ') {
+10                word.insert(0, ch); // Prepend character to reverse the word
+11            } else {
+12                result.append(word).append( );
+13                word.setLength(0); // Reset word builder
+14            }
+15        }
+16        
+17        // Append the last word which didn't hit a space
+18        result.append(word);
+19
+20        return result.toString();
+21    }
+22}
