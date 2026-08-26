e724ac642ee9ce6a76d14051db19fadc437dfb99
Subashree S <subashreevjc@gmail.com>
2026-08-26 23:11:32 +0530

    [0000] [Time Beats: 100%] [Memory Beats: 100.00%] - Leet2Hub

---
+++ AMAZON/0000. Time Needed To Buy Tickets/0000. Time Needed To Buy Tickets.java
@@ -0,0 +1,15 @@
+public int timeRequiredToBuy(int[] tickets, int k) {
+        int total=0;
+        while (true){
+            for(int i=0;i<tickets.length;i++){
+                if(tickets[i]>0){
+                    tickets[i]--;
+                    total++;
+                }
+                if(tickets[k]==0){
+                    return total;
+                }
+
+            }
+        }
+    }
